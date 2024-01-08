b2281d560bdc86ed2c9a753075f3585e7a7422ca
武田 憲太郎 <takeda@youmind.jp>
2024-01-08 17:15:15 +0900

    generating html

--- src/reveal.processor.ts
+++ src/reveal.processor.ts
@@ -16,9 +16,9 @@ export async function revealProcessorCallback(this: Hexo, file: Hexo.Box.File) {
   }
 
   if (file.type === "update" || file.type === "create") {
-    const body = await file.read({encoding: "utf-8"});
-    await mkdir(dirname(filename), {recursive: true});
-    await writeFile(filename, body);
+    const body = await file.read({ encoding: "utf-8" });
+    await mkdir(dirname(filename), { recursive: true });
+    await writeFile(filename, createHtml(body.toString()));
 
     return;
   }
@@ -28,3 +28,52 @@ export async function revealProcessorCallback(this: Hexo, file: Hexo.Box.File) {
 
 const getFilename = (hexo: Hexo, file: Hexo.Box.File) =>
   join(hexo.public_dir, "slide", file.params[1] + ".html");
+
+const createHtml = (markdown: string) => `
+<!doctype html>
+<html lang="en"> <!-- TODO lang -->
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <title>reveal.js</title> <!-- TODO title-->
+
+    <!-- TODO css-->
+    <link rel="stylesheet" href="/reveal.js/dist/reset.css">
+    <link rel="stylesheet" href="/reveal.js/dist/reveal.css">
+    <link rel="stylesheet" href="/reveal.js/dist/theme/black.css">
+
+    <!-- Theme used for syntax highlighted code -->
+    <link rel="stylesheet" href="/reveal.js/plugin/highlight/monokai.css">
+  </head>
+  <body>
+    <div class="reveal">
+      <div class="slides">
+        <section data-markdown>
+          <textarea data-template>
+
+${markdown}
+
+          </textarea>
+        </section>
+      </div>
+    </div>
+
+    <script src="/reveal.js/dist/reveal.js"></script>
+    <script src="/reveal.js/plugin/notes/notes.js"></script>
+    <script src="/reveal.js/plugin/markdown/markdown.js"></script>
+    <script src="/reveal.js/plugin/highlight/highlight.js"></script>
+    <script>
+      // More info about initialization & config:
+      // - https://revealjs.com/initialization/
+      // - https://revealjs.com/config/
+      Reveal.initialize({
+        hash: true,
+
+        // Learn about plugins: https://revealjs.com/plugins/
+        plugins: [ RevealMarkdown, RevealHighlight, RevealNotes ]
+      });
+    </script>
+  </body>
+</html>
+`;
