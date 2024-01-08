7aadbd147445079646982030616d25abf2e738ac
武田 憲太郎 <takeda@youmind.jp>
2024-01-08 18:16:39 +0900

    Add unit test for reveal.generator module

---
+++ tests/reveal.generator.spec.ts
@@ -0,0 +1,29 @@
+jest.mock("fs", () => ({
+  createReadStream: jest.fn(),
+  realpathSync: jest.fn().mockReturnValue('/test'),
+}));
+
+jest.mock("glob", () => ({
+  globSync: jest.fn(),
+}));
+
+import fs from "fs";
+import glob from "glob";
+import {revealGeneratorCallback} from "../src/reveal.generator";
+
+describe("reveal.generator", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should generate glob of specified file', ()=>{
+    glob.globSync = jest.fn().mockReturnValue(['/test/foo', '/test/bar/baz']);
+
+    const result = revealGeneratorCallback();
+    result.map((item) => item.data());
+
+    expect(fs.realpathSync).toHaveBeenCalled();
+    expect(fs.createReadStream).toHaveBeenCalledWith('/test/foo');
+    expect(fs.createReadStream).toHaveBeenCalledWith('/test/bar/baz');
+  });
+});;
