18456b508abaca448a5d7e6fdee09c7e28a31885
武田 憲太郎 <takeda@youmind.jp>
2024-01-09 12:17:26 +0900

    chore: formatting

--- tests/reveal.generator.spec.ts
+++ tests/reveal.generator.spec.ts
@@ -1,7 +1,7 @@
 jest.mock("fs", () => ({
   createReadStream: jest.fn(),
   existsSync: jest.fn().mockReturnValue(true),
-  realpathSync: jest.fn().mockReturnValue('/test'),
+  realpathSync: jest.fn().mockReturnValue("/test"),
 }));
 
 jest.mock("glob", () => ({
@@ -10,21 +10,21 @@ jest.mock("glob", () => ({
 
 import fs from "fs";
 import glob from "glob";
-import {revealGeneratorCallback} from "../src/reveal.generator";
+import { revealGeneratorCallback } from "../src/reveal.generator";
 
 describe("reveal.generator", () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('should generate glob of specified file', ()=>{
-    glob.globSync = jest.fn().mockReturnValue(['/test/foo', '/test/bar/baz']);
+  it("should generate glob of specified file", () => {
+    glob.globSync = jest.fn().mockReturnValue(["/test/foo", "/test/bar/baz"]);
 
     const result = revealGeneratorCallback();
     result.map((item) => item.data());
 
     expect(fs.realpathSync).toHaveBeenCalled();
-    expect(fs.createReadStream).toHaveBeenCalledWith('/test/foo');
-    expect(fs.createReadStream).toHaveBeenCalledWith('/test/bar/baz');
+    expect(fs.createReadStream).toHaveBeenCalledWith("/test/foo");
+    expect(fs.createReadStream).toHaveBeenCalledWith("/test/bar/baz");
   });
-});;
+});
